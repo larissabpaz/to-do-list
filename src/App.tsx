ca7950ca9cef7e1212424530f3ee1be0b3568686
Larissa <larissabragadapaz@gmail.com>
2024-10-03 15:10:46 -0300

    WIP: organizando as telas

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './components/UserRegister/SignUp';
 import Login from './components/Login/Login';
--- src/components/UserRegister/SignUp.tsx
+++ src/components/UserRegister/SignUp.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Typography, TextField, Button } from "@mui/material";
 import { Container, Box } from "@mui/system";
 
